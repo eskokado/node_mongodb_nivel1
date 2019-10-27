a2d000056956ddf75b8e4ff479e3b06edf949625
Edson Shideki Kokado <eskokado@gmail.com>
2019-10-27 11:51:25 -0300

    Configurando Template Engine Mustache

--- routes/index.js
+++ routes/index.js
@@ -2,33 +2,41 @@ const express = require('express');
 
 const router = express.Router();
 router.get('/', (req, res) => {
-  res.json(req.query);
-  
-  // GET: req.query
-  // POST: req.body
-  // PARAMETROS DA URL: req.param
-
-    //SEND
-    //JSON
-
-//    res.send(`Olá, ${req.query.nome}, você tem ${req.query.idade} ano(s)`);
+  let obj = {
+    'nome': 'Edson Shideki Kokado',
+    'idade': 53
+  }
+  res.render('home', obj);
 });
+
+// router.get('/', (req, res) => {
+//   // res.json(req.query);
+  
+//   // GET: req.query
+//   // POST: req.body
+//   // PARAMETROS DA URL: req.param
+
+//     //SEND
+//     //JSON
+
+// //    res.send(`Olá, ${req.query.nome}, você tem ${req.query.idade} ano(s)`);
+// });
 
 //router.get('/posts/:id', (req, res) => {
 //    let id = req.params.id;
 
 //    res.send(`ID do post: ${id}`);
-router.get('/posts/:slug', (req, res) => {
-   let slug = req.params.slug;
+// router.get('/posts/:slug', (req, res) => {
+//    let slug = req.params.slug;
 
-    // Titulo: seja bem vindo
-    // Slug: seja-bem-vindo
+//     // Titulo: seja bem vindo
+//     // Slug: seja-bem-vindo
 
-   res.send(`SLUG do post: ${slug}`);
-});
+//    res.send(`SLUG do post: ${slug}`);
+// });
 
-router.get('/sobre', (req, res) => {
-    res.send('Página SOBRE')
-});
+// router.get('/sobre', (req, res) => {
+//     res.send('Página SOBRE')
+// });
 
 module.exports = router;
